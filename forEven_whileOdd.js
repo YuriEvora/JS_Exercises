f2bcaae44d040a452690f06bd729063904ca0e39
YuriEvora <40434330+YuriEvora@users.noreply.github.com>
2020-06-09 20:19:16 -0100

    Create forEven_whileOdd.js
    This script shows all even numbers till 50 with for loop and odd numbers with while loop.

---
+++ forEven_whileOdd.js
@@ -0,0 +1,17 @@
+console.log("Even Numbers:");
+
+for (let even = 0; even <= 50; even++) {
+    if(even % 2 == 0) { 
+        console.log(even);
+    }
+}
+
+let odd = 0;
+console.log("Odd Numbers:");
+
+while (odd <= 50) {
+    if(odd % 2 == 1) { 
+        console.log(odd);
+    }
+    odd++;
+}
