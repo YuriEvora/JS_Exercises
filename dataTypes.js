46cc029341e992663a6209659bbc44b4e45c475c
YuriEvora <40434330+YuriEvora@users.noreply.github.com>
2020-06-09 19:54:13 -0100

    Create dataTypes.js
    This script shows all data types available in JS.

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+// Number
+let num  = 10;
+console.log(num);
+console.log(typeof num);
+
+// String
+let dez  = "10";
+console.log(dez);
+console.log(typeof dez);
+
+// Boolean
+let bool_  = dez == num;
+console.log(bool_);
+console.log(typeof bool_);
+
+// Undefined
+let undefinedVar_ = undefined;
+console.log(undefinedVar_);
+console.log(typeof undefinedVar_);
+
+// Null
+let null_  = null;
+console.log(null_);
+console.log(typeof null_);
